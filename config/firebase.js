ff4dd4398b0885bc43ca9dea1e73e651057b9e87
Zihadul <zihadul10101@gmail.com>
2026-04-18 00:03:05 +0600

    firebase env setup

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,20 @@
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../bus-tracking-3ddab-firebase-adminsdk-fbsvc-9942eab2a3.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
+
 const admin = require("firebase-admin");
-const serviceAccount = require("../bus-tracking-3ddab-firebase-adminsdk-fbsvc-14a71e8298.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  })
 });
 
 module.exports = admin;
