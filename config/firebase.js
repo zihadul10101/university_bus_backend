e52e7bee3b926ac588d39cbd56db2f793b4e9869
Zihadul <zihadul10101@gmail.com>
2026-04-18 00:15:07 +0600

    first

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,3 @@
-// const admin = require("firebase-admin");
-// const serviceAccount = require("../bus-tracking-3ddab-firebase-adminsdk-fbsvc-9942eab2a3.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
-// module.exports = admin;
 
 const admin = require("firebase-admin");
 
