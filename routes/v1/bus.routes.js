a78e046a29771c33dffc2ab6c301f72da7ffe448
Zihadul <zihadul10101@gmail.com>
2026-05-09 22:14:17 +0600

    bus route updated

--- routes/v1/bus.routes.js
+++ routes/v1/bus.routes.js
@@ -16,14 +16,18 @@ router.post('/:busId/add-trip', authMiddleware(['super_admin', 'sub_admin']),add
  router.delete('/:busId',authMiddleware(['super_admin', 'sub_admin']), deleteBus);
 
 // // Read-only routes accessible by admin + student
- router.get("/current-trips",authMiddleware(['super_admin', 'sub_admin','student','driver']), getCurrentTrips);
- router.get("/trip-status", authMiddleware(['super_admin', 'sub_admin','student','driver']),getTripsStatus);
+ //router.get("/current-trips",authMiddleware(['super_admin', 'sub_admin','student','driver']), getCurrentTrips);
+ router.get("/current-trips", getCurrentTrips);
+ //router.get("/trip-status", authMiddleware(['super_admin', 'sub_admin','student','driver']),getTripsStatus);
+ router.get("/trip-status",getTripsStatus);
 
 
 
 
- router.get('/all-bus',authMiddleware(['super_admin', 'sub_admin','student','driver']), getAllBuses);
-router.get('/:busId',authMiddleware(['super_admin', 'sub_admin','student','driver']),getBusById);
+ //router.get('/all-bus',authMiddleware(['super_admin', 'sub_admin','student','driver']), getAllBuses);
+ router.get('/all-bus', getAllBuses);
+//router.get('/:busId',authMiddleware(['super_admin', 'sub_admin','student','driver']),getBusById);
+router.get('/:busId',getBusById);
 
 
 module.exports = router;
