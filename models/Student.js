d0e695c13be9b8fee2688b0de01df56e28766de7
Zihadul <zihadul10101@gmail.com>
2026-07-28 14:22:44 +0600

    without otp

--- models/Student.js
+++ models/Student.js
@@ -1,30 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const StudentSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     lowercase: true
-//   },
-
-//   password: { type: String, required: true },
-
-//   departmentName: { type: String, required: true },
-
-//   mobileNumber: { type: String, required: true },
-//   role: { type: String, default: 'student' },
-//   otp: Number,
-//   otpExpire: Date,
-
-//   lastLogin: { type: Date }
-
-// }, { timestamps: true });
-
-// module.exports = mongoose.model('Student', StudentSchema);
-const mongoose = require('mongoose');
 
 const StudentSchema = new mongoose.Schema({
   name: { type: String, required: true },
